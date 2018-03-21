9045ffe9c69cb8d50328d0942166307669ca7ace
Michael <schomburg.michael90@gmail.com>
2018-03-21 14:15:59 +0100

    layout and gallery plugin
    gallery plugin and placing of images and search bar

---
+++ js/script.js
@@ -0,0 +1,13 @@
+lightbox.option({
+      'showImageNumberLabel' : false,
+   })
+
+
+   // function addToAlbum($link) {
+   //   self.album.push({
+   //     cap: $link.attr('data-caption'),
+   //     // alt: $link.attr('data-alt'),
+   //     // link: $link.attr('href'),
+   //     // title: $link.attr('data-title') || $link.attr('title')
+   //   });
+   // }
